b9bf62d0ec5750c9c81dcaf264e73f57056878c1
abrammer <a1brammer@gmail.com>
2020-01-20 21:26:47 +0000

    try and force install numpy first

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
-from numpy.distutils.core import setup, Extension
+try:
+    from numpy.distutils.core import setup, Extension
+except ImportError:
+    from setuptools import dist
+    dist.Distribution().fetch_build_eggs(['numpy>=1.10'])
+    from numpy.distutils.core import setup, Extension
+
 
 lib = Extension(name='gripy.libg2', sources=['gripy/libg2.f90'])
 
 
 setup (name = 'gripy',
        version = '0.0',
-       install_requires=['numpy', 'pupygrib',],
+       install_requires=['numpy',],
+       setup_requires=['numpy',],
        ext_modules = [lib,],
        description = '''Grib Reader In PYthon.,
            'Developmental repo for a python based grib reader. Replacing external deps with python''',
