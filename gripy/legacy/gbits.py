94149805a4c03f3a2e9e0a8839193819d2f47484
Alan Brammer <a1brammer@gmail.com>
2020-02-09 16:12:03 -0700

    move unused code to legacy

--- gripy/legacy/gbits.py
+++ gripy/legacy/gbits.py
@@ -15,6 +15,7 @@ def ishft(val, bitcnt):
     print(np.unpackbits(np.uint8(retval)))
     return retval
 
+
 def iand(val1, val2):
     print("iand")
     print(np.unpackbits(np.uint8(val1)))
@@ -27,6 +28,7 @@ def ior(val1, val2):
     print(np.unpackbits(np.uint8(val1)))
     print(np.unpackbits(np.uint8(val2)))
     return val1 | val2
+
 
 def ones(n):
     print(f"{n}: {(2<<n) - 1}")
@@ -44,10 +46,10 @@ def gbits(buff, pos, nbits: int, nskip:int, n_num:int):
         ibit = int(nbit % 8)
         nbit = nbit + nbyte + nskip
         print(f"nbit: {nbit}")
-#       first byte
+        #       first byte
         tbit = np.min([bitcnt, 8-ibit])
         itmp = iand((buff[index]), ones(8-ibit))
-        
+
         if (tbit != 8-ibit):
             itmp = ishft(itmp, tbit-8+ibit)
 
@@ -55,7 +57,7 @@ def gbits(buff, pos, nbits: int, nskip:int, n_num:int):
         bitcnt = bitcnt - tbit
         print(f"index: {index}")
         print(f"bitcnt: {bitcnt}")
-#       now transfer whole bytes
+        #       now transfer whole bytes
         while (bitcnt == 8):
             itmp = ior(ishft(itmp, 8), (buff[index]))
             bitcnt = bitcnt - 8
@@ -63,11 +65,11 @@ def gbits(buff, pos, nbits: int, nskip:int, n_num:int):
 
         print(f"index: {index}")
         print(f"bitcnt: {bitcnt}")
-#       get data from last byte
+        #       get data from last byte
         if (bitcnt > 0):
-            itmp = ior(ishft(itmp, bitcnt), 
+            itmp = ior(ishft(itmp, bitcnt),
                        iand(ishft(buff[index], -(8-bitcnt)), ones(bitcnt))
                       )
-    
+
         iout.append(itmp)
     return np.array(iout, dtype=np.int32)
