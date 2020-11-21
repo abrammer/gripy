e0c84ab9b36ad0474bb4873adeb07858dc8cfb16
Alan Brammer <a1brammer@gmail.com>
2020-11-20 18:03:04 -0700

    Feat/ include section 7 decoding with values property and section 6 bitmap support (#9)
    * initial support for section 7 and addition of a values property as well as section 6 bitmap support
    * unrelated mac-os tests failure on GitHub workflows.  Locals tests and build on Mac are fine...

--- gripy/libg2.f90
+++ gripy/libg2.f90
@@ -134,7 +134,7 @@
     integer,allocatable :: gref(:),gwidth(:),glen(:)
     real :: ref,bscale,dscale,rmiss1,rmiss2
     ! real :: fldo(6045)
-    integer :: totBit, totLen
+    integer :: totBit, totLen, non
     !f2py intent(in) cpack
     !f2py intent(in) len
     !f2py intent(in) lensec
@@ -146,7 +146,7 @@
     ier=0
     ! print *,'IDRSTMPL: ',(idrstmpl(j),j=1,16)
     ieee = idrstmpl(1)
-    call rdieee(ieee,ref,1)
+    call rdieee(ieee, ref, 1)
     bscale = 2.0**real(idrstmpl(2))
     dscale = 10.0**real(-idrstmpl(3))
     nbitsgref = idrstmpl(4)
@@ -217,7 +217,7 @@
         ival2=0
         minsd=0
       endif
-    ! print *,'SDu ',ival1,ival2,minsd,nbitsd
+      ! print *,'SDu ',ival1,ival2,minsd,nbitsd
     endif
     !
     !  Extract Each Group's reference value
@@ -281,11 +281,12 @@
     enddo
     if (totLen .NE. ndpts) then
       ier=1
-      ! print*, totLen, ndpts
+      ! print*, totLen, ".NE.", ndpts
       return
     endif
     if ( (totBit/8) .GT. lensec) then
       ier=1
+      ! print*, totBit/8, ".GT.", lensec
       return
     endif
     !
@@ -359,6 +360,7 @@
     !  If using spatial differences, add overall min value, and
     !  sum up recursively
     !
+    ! print*,' idrstmpl(17)', idrstmpl(17)
     if (idrsnum.eq.3) then         ! spatial differencing
       if (idrstmpl(17).eq.1) then      ! first order
           ifld(1)=ival1
@@ -389,8 +391,8 @@
     !
     !  Scale data back to original form
     !
-    !print*, MAXVAL(ifld), MINVAL(ifld)
-    !print *,'SAGT: ',ref,bscale,dscale
+    ! print*, MAXVAL(ifld), MINVAL(ifld)
+    ! print *,'SAGT: ',ref,bscale,dscale
     if ( idrstmpl(7).eq.0 ) then        ! no missing values
       do n=1,ndpts
         fld(n)=((real(ifld(n))*bscale)+ref)*dscale
@@ -425,7 +427,7 @@
   end subroutine comunpack
 
 
-  subroutine rdieee(rieee,a,num)
+  subroutine rdieee(rieee, a, num)
     !                .      .    .                                       .
     ! SUBPROGRAM:    rdieee
     !   PRGMMR: Gilbert         ORG: W/NP11    DATE: 2000-05-09
