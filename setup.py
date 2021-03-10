fdbb9ff3d4855cff742274c5da3abf1ade57b104
Alan Brammer <alan.brammer@colostate.edu>
2021-03-09 20:19:28 -0700

    try moving everything to setup cfg, mostly

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import setuptools
-from distutils.core import setup
 
 from distutils.command.sdist import sdist
 from numpy.distutils.core import setup, Extension
@@ -7,28 +6,7 @@ from numpy.distutils.core import setup, Extension
 
 lib = Extension(name='gripy.libg2', sources=['gripy/libg2.f90'])
 
-
 setup(
-    name='gripy',
-    author='abrammer',
-    author_email='alan.brammer@gmail.com',
-    url='https://github.com/abrammer/gripy',
-    version="0.0.0a0",
-    install_requires=[
-        'numpy',
-    ],
-    setup_requires=[
-        'numpy',
-    ],
-    ext_modules=[
-        lib,
-    ],
-    description='''Grib Reader In PYthon.,
-            Developmental repo for a python based grib reader.
-            Replacing external deps with python''',
-    packages=['gripy', 'gripy.tables', 'gripy.legacy'],
-    package_data={
-        "gripy.tables": ["*/*/*.json"],
-    },
+    ext_modules=[lib],
     cmdclass={'sdist': sdist},
 )
