d3ba7ed972cd4097e9548b2ea86bd1e843fb30bc
Alan Brammer <a1brammer@gmail.com>
2022-07-14 03:26:55 +0000

    formatting

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ from numpy.distutils.core import setup, Extension
 
 
 f90flags = []
-if int(subprocess.check_output(['gcc', '-dumpversion'])) >= 10:
-    f90flags+=["-fallow-argument-mismatch"]
+if int(subprocess.check_output(["gcc", "-dumpversion"])) >= 10:
+    f90flags += ["-fallow-argument-mismatch"]
 
-lib = Extension(name='gripy.libg2',
-    sources=['gripy/libg2.f90'], 
+lib = Extension(
+    name="gripy.libg2",
+    sources=["gripy/libg2.f90"],
     extra_f90_compile_args=f90flags,
-    )
+)
 
 
 setup(
-    ext_modules=[lib, ],
-    cmdclass={'sdist': sdist},
+    ext_modules=[
+        lib,
+    ],
+    cmdclass={"sdist": sdist},
 )
