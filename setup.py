520eae56aaae4b9c0711a29608cb754aebf39e6d
Alan Brammer <a1brammer@gmail.com>
2022-07-14 03:25:51 +0000

    add gcc conditional flag for install

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-import setuptools
+import subprocess
 from distutils.core import setup
 
 from distutils.command.sdist import sdist
 from numpy.distutils.core import setup, Extension
 
 
-lib = Extension(name='gripy.libg2', sources=['gripy/libg2.f90'], extra_f90_compile_args=["-fallow-argument-mismatch"])
+f90flags = []
+if int(subprocess.check_output(['gcc', '-dumpversion'])) >= 10:
+    f90flags+=["-fallow-argument-mismatch"]
+
+lib = Extension(name='gripy.libg2',
+    sources=['gripy/libg2.f90'], 
+    extra_f90_compile_args=f90flags,
+    )
 
 
 setup(
