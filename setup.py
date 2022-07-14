1e6063a8100d17a4d1583fd9e733ba2079b131d3
Alan Brammer <a1brammer@gmail.com>
2022-07-14 03:08:24 +0000

    Add devcontainer configs and fixup fortran compiler issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.command.sdist import sdist
 from numpy.distutils.core import setup, Extension
 
 
-lib = Extension(name='gripy.libg2', sources=['gripy/libg2.f90'])
+lib = Extension(name='gripy.libg2', sources=['gripy/libg2.f90'], extra_f90_compile_args=["-fallow-argument-mismatch"])
 
 
 setup(
